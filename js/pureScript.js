310ed597f16810bd56dddf563ad3c1ce5db594aa
Tamim Talukdar <s163.tamim@gmail.com>
2024-02-08 19:42:29 +0600

    created navbar with responsive

---
+++ js/pureScript.js
@@ -0,0 +1,16 @@
+const humberger = document.getElementById("humberger")
+const closeBar = document.getElementById("close");
+const navWrapper = document.querySelector(".nav-wrapper")
+
+humberger.addEventListener("click", function(){
+    humberger.style.display = "none";
+    closeBar.style.display = "block"
+    navWrapper.style.left = "0px";
+});
+
+closeBar.addEventListener("click", function(){
+    humberger.style.display = "block";
+    closeBar.style.display = "none";
+    navWrapper.style.left = "-40rem";
+});
+
