32209bb5dc26f38c68d50c971e99ec9ae39ca67d
Tamim Talukdar <s163.tamim@gmail.com>
2024-02-08 20:35:58 +0600

    fixed a simple logic

--- js/pureScript.js
+++ js/pureScript.js
@@ -1,15 +1,15 @@
 const humberger = document.getElementById("humberger")
 const closeBar = document.getElementById("close");
-const navWrapper = document.querySelector(".nav-wrapper")
+const navWrapper = document.querySelector(".nav-wrapper");
 
 humberger.addEventListener("click", function(){
     humberger.style.display = "none";
-    closeBar.style.display = "block"
+    closeBar.style.display = "block";
     navWrapper.style.left = "0px";
 });
 
 closeBar.addEventListener("click", function(){
-    humberger.style.display = "block";
+    humberger.classList.add("block");
     closeBar.style.display = "none";
     navWrapper.style.left = "-40rem";
 });
