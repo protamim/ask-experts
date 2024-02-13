aca42f0bd01c5f9623b447895711ae492e278bd1
Tamim Talukdar <s163.tamim@gmail.com>
2024-02-13 16:15:10 +0600

    Client review section

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+  // loop: true,
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+    disabledClass: "swiper-button-disabled"
+  },
+});
